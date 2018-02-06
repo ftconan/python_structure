eaebcaa8139ed7e1b34a1062924bfc080d829d4a
冯涛 <1508907793@qq.com>
2018-02-06 14:45:05 +0800

    双链表节点类

--- array_linked_list/node.py
+++ array_linked_list/node.py
@@ -13,6 +13,12 @@ class Node(object):
         self.next = next
 
 
+class TwoWayNode(Node):
+    def __init__(self, data, previous=None, next=None):
+        Node.__init__(self, data, next)
+        self.previous = previous
+
+
 def test_node():
     """
     Adde five nodes t the beginning of the linked structure
@@ -27,5 +33,21 @@ def test_node():
         head = head.next
 
 
+def test_two_way_node():
+    # Create a doubly linked structure with one node
+    head = TwoWayNode(1)
+    tail = head
+    # Add four nodes to the end of the doubly linked structure
+    for data in range(2, 6):
+        tail.next = TwoWayNode(data, tail)
+        tail = tail.next
+    # Print the contents of the linked structure in reverse order
+    probe = tail
+    while probe != None:
+        print probe.data
+        probe = probe.previous
+
+
 if __name__ == '__main__':
     test_node()
+    test_two_way_node()
